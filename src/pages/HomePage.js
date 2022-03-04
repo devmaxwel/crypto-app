1ce1255dad304f02441046ff031cd529b98423a0
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-03-04 11:08:26 +0300

    adsense addition

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Banner from '../components/banner/Banner';
-import CoinsTable from '../components/CoinsTable';
+import React from "react";
+import Banner from "../components/banner/Banner";
+import CoinsTable from "../components/CoinsTable";
+import AdSense from "react-adsense";
 
 const HomePage = () => {
-    return (
-        <div>
-           <Banner />
-           <CoinsTable />
-        </div>
-    )
-}
+  return (
+    <div>
+      <AdSense.Google 
+      client={process.env.REACT_APP_KEY} 
+      slot={process.env.ID} 
+      style={{ display: 'block' }}
+      layout='in-article'
+      format='fluid' />
+      <Banner />
+      <CoinsTable />
+    </div>
+  );
+};
 
 export default HomePage;
